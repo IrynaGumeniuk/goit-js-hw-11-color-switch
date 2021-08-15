1e87eeb21e0b29fb33d5467ad5331a8622728d43
Iryna Gumeniuk <iriwkagumenuk@gmail.com>
2021-08-15 12:56:01 +0200

    Add parcel docs

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 // import colors from './colors.json';
 
+const startBtn = document.querySelector('[data-action="start"]');
+const stopBtn = document.querySelector('[data-action="stop"]');
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+const startBtnIntervalId = setInterval(() => console.log('Приветик'), 1000);
+
+function onStartBtnClick() {
+
+}
+
+function onStopBtnClick() {
+    clearInterval(startBtnIntervalId);
+};
+
+
+const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
